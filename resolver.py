a9508834d5b46d5c8a4230ed8a721312db503fa6
niainamandis <debian@debian>
2026-09-03 17:20:14 -0700

    feat: extract and parse DNS query fields into dictionary

--- resolver.py
+++ resolver.py
@@ -1,11 +1,40 @@
 import socket
-import dnslib
+import dnslib 
+
+def parse_dns(message):
+  # parseamos primero con dnslib
+  d = dnslib.DNSRecord.parse(message)
+
+  # nombre del dominio consultado
+  qname = d.questions[0].qname
+
+  # contadores: se obtienen midiendo el tamaño de las listas
+  ancount = len(d.rr)     # respuestas
+  nscount = len(d.auth)   # registros de autoridad
+  arcount = len(d.ar)     # registros adicionales
+
+  # secciones
+  answer_section = d.rr       
+  authority_section = d.auth  
+  additional_section = d.ar  
+
+  # almacenamos y retornamos un diccionario 
+  return {
+    "Qname": qname,
+    "ANCOUNT": ancount,
+    "NSCOUNT": nscount, 
+    "ARCOUNT": arcount,
+    "Answer": answer_section, 
+    "Authority": authority_section,
+    "Additional": additional_section,
+  }
 
 if __name__ == "__main__":
   # setup inicial del proxy
   buff_size = 8129 
-  IP_VM = "192.168.64.2" # amandis 192.168.64.2
-                         # dani 10.0.2.15
+  IP_VM = "127.0.1.1" # amandis 192.168.64.2
+                      # dani 10.0.2.15
+                      # localhost 127.0.1.1
   socket_address = (IP_VM, 8000)
 
   print(f"Creando socket servidor en {IP_VM}:8000")
@@ -20,4 +49,8 @@ if __name__ == "__main__":
     # recibimos el mensaje junto con su origen 
     message, address = dgram_socket.recvfrom(buff_size)
 
-    print(f"Mensaje obtenido de {address}: {message}")
+    # parseamos el mensaje
+    parsed_req = parse_dns(message)
+
+    print(f"Mensaje obtenido de {address}")
+    print(f"Contenido: {parsed_req}")
